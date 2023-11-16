4908afea6583b3ee052a2a19b38473668320a0b7
zchtaibi <zchtaibi@e2r10p5.1337.ma>
2023-11-16 15:11:50 +0100

    push Libft

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,20 +6,20 @@
 /*   By: zchtaibi <zchtaibi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 16:34:25 by zchtaibi          #+#    #+#             */
-/*   Updated: 2023/11/15 21:56:02 by zchtaibi         ###   ########.fr       */
+/*   Updated: 2023/11/16 00:40:24 by zchtaibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstiter(t_list *lst, void (*f)(void *))
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-    if (!lst || !f)
-        return ;
-    while (lst)
-    {
-        if (lst->content)
-            f(lst->content);
-        lst = lst->next;
-    }
+	if (!lst || !f)
+		return ;
+	while (lst)
+	{
+		if (lst->content)
+			f(lst->content);
+		lst = lst->next;
+	}
 }
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,32 +6,32 @@
 /*   By: zchtaibi <zchtaibi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 16:26:29 by zchtaibi          #+#    #+#             */
-/*   Updated: 2023/11/15 22:17:16 by zchtaibi         ###   ########.fr       */
+/*   Updated: 2023/11/16 11:42:27 by zchtaibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    t_list *new_lst;
-    
-    if (!lst || !f || !del)
-        return (NULL);
-    new_lst = NULL;
-    while (lst)
-    {
-        void *content_result = f(lst->content);
-        t_list *new_node = ft_lstnew(content_result);
+	t_list	*new_lst;
+	t_list	*new_node;
+	void	*content_result;
 
-        if (!new_node)
-        {
-            ft_lstiter(new_lst, del);
-            return NULL;
-        }
-        ft_lstadd_back(&new_lst, new_node);
-        lst = lst->next;
-    }
-    return new_lst;
-    
+	if (!lst || !f || !del)
+		return (NULL);
+	new_lst = NULL;
+	while (lst)
+	{
+		content_result = f(lst->content);
+		new_node = ft_lstnew(content_result);
+		if (!new_node)
+		{
+			ft_lstiter(new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, new_node);
+		lst = lst->next;
+	}
+	return (new_lst);
 }
